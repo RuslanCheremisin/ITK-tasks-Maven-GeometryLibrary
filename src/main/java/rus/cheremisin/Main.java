371a4b7742aef0cd14d62c9ae8e936e2596e4a7c
rus <ruslan.cheremisin@gmail.com>
2026-04-07 09:55:02 +1000

    Maven

---
+++ src/main/java/rus/cheremisin/Main.java
@@ -0,0 +1,27 @@
+package rus.cheremisin;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(4, 6);
+        Triangle triangle = new Triangle(3, 4, 5);
+
+        System.out.println("Круг c радиусом = " + circle.getRadius());
+        System.out.println("Площадь: " + circle.getArea());
+        System.out.println("Периметр: " + circle.getPerimeter());
+
+        System.out.println("=================================");
+
+        System.out.println("Прямоугольник " + rectangle.getWidth() +
+                " Х " + rectangle.getHeight());
+        System.out.println("Площадь: " + rectangle.getArea());
+        System.out.println("Периметр: " + rectangle.getPerimeter());
+
+        System.out.println("=================================");
+        
+        System.out.println("Треугольник " + triangle.getSideA() +
+                " Х " + triangle.getSideB() + " Х " + triangle.getSideC());
+        System.out.println("Площадь: " + triangle.getArea());
+        System.out.println("Периметр: " + triangle.getPerimeter());
+    }
+}
